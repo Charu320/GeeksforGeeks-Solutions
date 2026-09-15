2c0ab48766b750ce549e56f8ffcaaa2847940196
Charu320 <charupulani2006@gmail.com>
2026-09-15 12:09:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Greatest of Three Numbers/greatest-of-three-numbers.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    // code here
+    if(a>b && a>c){
+    cout<<a;
+    }
+    else if( b>c && b>a){
+        cout<<b;
+    }
+    else
+    cout<<c;
+    
+
+    return 0;
+}
